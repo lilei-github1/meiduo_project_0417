a7a3af6c4acd3955531cc196e1a1f33f67fd3755
li <1875727687@qq.com>
2020-06-05 22:17:14 +0800

    用户登录之QQ登录-openid已绑定用户-openid未绑定用户-openid绑定用户逻辑-接收和校验参数

--- meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/apps/oauth/views.py
@@ -6,6 +6,9 @@ from django import http
 from django.contrib.auth import login
 from apps.oauth.models import OAuthQQUser
 from apps.oauth.utils import generate_access_token_openid, check_access_token_openid
+import json, re
+from apps.users.models import User
+from django_redis import get_redis_connection
 # Create your views here.
 class QQUserView(View):
     """
@@ -55,7 +58,62 @@ class QQUserView(View):
             response = http.JsonResponse({'code': 0, 'errmsg': 'ok'})
             response.set_cookie('username',oauth_model.user.username,max_age=3600*24*14)
             return response
-        pass
+    def post(self, request):
+        """实现openid绑定用户的逻辑"""
+        # 接收参数
+        json_dict = json.loads(request.body.decode())
+        mobile = json_dict.get('mobile')
+        password = json_dict.get('password')
+        sms_code_client = json_dict.get('sms_code')
+        access_token = json_dict.get('access_token')
+
+        # 校验参数
+        if not all([mobile, password, sms_code_client, access_token]):
+            return http.JsonResponse({'code': 400, 'errmsg': '缺少必传参数'})
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.JsonResponse({'code': 400, 'errmsg': '参数mobile有误'})
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return http.JsonResponse({'code': 400, 'errmsg': '参数password有误'})
+        # 校验短信验证码
+        redis_conn = get_redis_connection('verify_code')
+        sms_code_server = redis_conn.get('sms_%s' % mobile) # sms_code_server是bytes
+        # 判断短信验证码是否过期
+        if not sms_code_server:
+            return http.JsonResponse({'code': 400, 'errmsg': '短信验证码失效'})
+        # 对比用户输入的和服务端存储的短信验证码是否一致
+        if sms_code_client != sms_code_server.decode():
+            return http.JsonResponse({'code': 400, 'errmsg': '短信验证码有误'})
+        # 校验openid
+        openid = check_access_token_openid(access_token)
+        if not openid:
+            return http.JsonResponse({'code': 400, 'errmsg': '参数openid有误'})
+
+        # 判断手机号对应的用户是否存在
+        try:
+            user = User.objects.get(mobile=mobile)
+        except User.DoesNotExist:
+            # 如果手机号对应的用户不存在，新建用户:这个时候新建的用户，手机号即是用户名，也是手机号
+            user = User.objects.create_user(username=mobile, password=password, mobile=mobile)
+        else:
+            # 如果手机号对应的用户已存在，校验密码
+            if not user.check_password(password):
+                return http.JsonResponse({'code': 400, 'errmsg': '密码有误'})
+
+        # 将上面得到的用户跟openid进行绑定即可
+        # create_user()：只有继承自AbstractUser的用户模型类才能去调用的，创建用户记录
+        # create()：凡是继承自Model的模型类都可以调用，用来创建记录
+        try:
+            OAuthQQUser.objects.create(user=user, openid=openid)
+        except Exception as e:
+            return http.JsonResponse({'code': 400, 'errmsg': 'QQ登录失败'})
+
+        # 实现状态保持
+        login(request=request, user=user)
+        response = http.JsonResponse({'code': 0, 'errmsg': 'OK'})
+        response.set_cookie('username', user.username, max_age=3600*24*14)
+
+        # 响应结果
+        return response
 class QQURLView(View):
     """提供QQ登录页面网址"""
     # GET:/qq/authorization/
